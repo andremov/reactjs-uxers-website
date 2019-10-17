55fd3930e33c865ecc058e00997493a3ed69f115
Andres-XL <amovilla1103@gmail.com>
2019-10-17 01:36:46 -0500

    build 1.0

---
+++ uxers/build/precache-manifest.83128c02dd49526ec3faa9e72c0c27a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e629999c99c9b1cc74ceae6852b3dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f344756e5d394bd7d00",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c5405912bb02b23878d",
+    "url": "/static/js/2.bd8cd816.chunk.js"
+  },
+  {
+    "revision": "8f344756e5d394bd7d00",
+    "url": "/static/js/main.912f737b.chunk.js"
+  },
+  {
+    "revision": "791341e9627036faf9d6",
+    "url": "/static/js/runtime-main.1fd6a666.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
