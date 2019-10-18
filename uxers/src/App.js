76406c646c4b3259b25df90ae6bdb716266202b9
Andres-XL <amovilla1103@gmail.com>
2019-10-18 00:14:50 -0500

    removed base css

--- uxers/src/App.js
+++ uxers/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './Style/App.scss';
 import {MainComponent} from "./Components/MainComponent";
 
 function App() {
   return (
-    <MainComponent />
+    <MainComponent/>
   );
 }
 
