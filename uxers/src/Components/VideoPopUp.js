289f64a147af98a29636a494dd2bedb9a9f2edb5
Andres-XL <amovilla1103@gmail.com>
2019-11-14 20:33:45 -0500

    quité el video

--- uxers/src/Components/VideoPopUp.js
+++ uxers/src/Components/VideoPopUp.js
@@ -1,23 +1,29 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import video from "../assets/fotovideo.png";
 
 export class VideoPopUp extends Component {
     render() {
-        return (
-            <div className='video-pop-up'>
-                <div className='top'>
-                    <div className='action'>
-                        <span>Ver</span>
-                        <span>video</span>
+        /*
+            return (
+                <div className='video-pop-up'>
+                    <div className='top'>
+                        <div className='action'>
+                            <span>Ver</span>
+                            <span>video</span>
+                        </div>
+                        <div className='line'>
+                            <div className='circle'> </div>
+                        </div>
                     </div>
-                    <div className='line'>
-                        <div className='circle'> </div>
+                    <div className='img'>
+                        <img src={video} alt='Fotito toda chevere'/>
                     </div>
                 </div>
-                <div className='img'>
-                    <img src={video} alt='Fotito toda chevere'/>
-                </div>
-            </div>
-        );
+            );
+        */
+        return(<Fragment>
+            
+        </Fragment>)
     }
+
 }
